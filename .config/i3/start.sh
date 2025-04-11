3ea5693e31753cae640e15324008594ada7552ef
aKqir24 <Akqir@protonmail.com>
2025-04-11 21:41:18 +0800

    Updated Config Files...

--- .config/i3/start.sh
+++ .config/i3/start.sh
@@ -5,8 +5,8 @@ xrandr --newmode "1152x864_60.00"   81.75  1152 1216 1336 1520  864 867 871 897 
 xrandr --addmode VGA1 "1152x864_60.00" ; xrandr --output VGA1 --mode "1152x864_60.00" &
 
 # Desktop, UI, Window Elements
-killall autotiling ; killall paperview ;
-paperview ~/Pictures/Wallpaper/Gif/\nature_girl 8 & autotiling &
+killall autotiling & killall paperview ; dunst &
+paperview ~/Pictures/Wallpaper/Gif/\nature_girl 8 & autotiling & dunst &
 
 polybar-msg cmd quit ; killall -q polybar &
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log ;
